ee9372723e9d11dab785875e679e10362a887196
Ahmad <ahmed.332022@gmail.com>
2024-01-16 17:55:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const menu = document.getElementById('kebab-menu');
+const menuIcon = document.getElementById('menu-icon');
+const menuContent = document.getElementById('menu-content');
+const likes = document.getElementById('likes');
+const counter = document.getElementById('counter');
+const article = document.getElementById("article");
+const tools = document.getElementById('tools');
+const colorText = document.getElementById('color-text');
+
+
+
+// show & hide menu
+function showHideMenu() {
+    if (menuIcon.classList.contains('fa-ellipsis-vertical')) {
+        menuIcon.classList.remove('fa-ellipsis-vertical');
+        menuIcon.classList.add('fa-ellipsis');
+        menuContent.style.display = "block";
+    }
+    else {
+        menuIcon.classList.remove('fa-ellipsis');
+        menuIcon.classList.add('fa-ellipsis-vertical');
+        menuContent.style.display = "none";
+    };
+}
+
+// increse number of likes
+function likesCounter() {
+    conterText = counter.innerHTML;
+    numLikes = Number(conterText) + 1;
+    counter.textContent = ' ' + numLikes + ' ';
+}
+
+
+// show pop up menu after selecting text
+function selectText(event) {
+    if (window.getSelection().toString().length > 0) {
+        const posX = event.clientX;
+        const posY = event.clientY;
+
+        tools.style.top = posY - 50;
+        tools.style.left = posX - 60;
+        tools.style.display = "block";
+    }
+    else {
+        tools.style.display = "none";
+    }
+}
+
+function colorSelctedText() {
+
+    document.body.style.setProperty('--selcolor', '#1bba1e');
+}
+
+
+// Event Listeners
+menu.addEventListener('click', showHideMenu);
+likes.addEventListener('click', likesCounter);
+article.addEventListener('mouseup', selectText);
+colorText.addEventListener('click', colorSelctedText);
+
+
+
